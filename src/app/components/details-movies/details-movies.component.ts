fad8a77ccf4caf9af3fd2de0e8f3228762ae9438
Mustafa Aga <mustafaaga23@gmail.com>
2022-08-23 16:23:30 +0200

    components

---
+++ src/app/components/details-movies/details-movies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-movies',
+  templateUrl: './details-movies.component.html',
+  styleUrls: ['./details-movies.component.scss']
+})
+export class DetailsMoviesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/details-reviews/details-reviews.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-reviews',
+  templateUrl: './details-reviews.component.html',
+  styleUrls: ['./details-reviews.component.scss']
+})
+export class DetailsReviewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
